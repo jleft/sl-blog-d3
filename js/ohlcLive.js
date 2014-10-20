d097502998ac739986203fa5ff19143030c2a31c
James Leftley <jleftley@scottlogic.com>
2014-10-20 15:38:09 +0100

    OHLC chart with changing data (without transitions)

--- js/ohlcLive.js
+++ js/ohlcLive.js
@@ -2,16 +2,29 @@ define([
     'd3',
     'moment',
     'components/sl',
-    'MockData',
-    'components/ohlcSeries'
-], function (d3, moment, sl, MockData) {
+    'components/ohlcSeries',
+    'moment-range'
+], function (d3, moment, sl) {
     'use strict';
 
-    var mockData = new MockData(0.1, 0.1, 100, 50, function (moment) {
-        return !(moment.day() === 0 || moment.day() === 6);
-    });
+    var dataset;
+    var datasetIndex = 20;
 
-    var data = mockData.generateOHLC(new Date(2014, 6, 1), new Date(2014, 8, 1));
+    d3.csv('../data/TestStreamingData.csv', function(d) {
+        return {
+            date: new Date(d.date),
+            open: +d.open,
+            high: +d.high,
+            low: +d.low,
+            close: +d.close 
+        };
+    }, function(error, rows) {
+        // Get all of the entries from the file
+        dataset = rows;
+
+        var data = processDatasetForChart(dataset);
+        drawAxesAndSeries(data);
+    });
 
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
         width = 660 - margin.left - margin.right,
@@ -23,7 +36,7 @@ define([
     var xAxis = d3.svg.axis()
         .scale(xScale)
         .orient('bottom')
-        .ticks(5);
+        .ticks(d3.time.minutes, 1);
 
     var yAxis = d3.svg.axis()
         .scale(yScale)
@@ -38,7 +51,7 @@ define([
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom);
 
-    // Ceate chart
+    // Create chart
     var g = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
@@ -50,53 +63,108 @@ define([
         .attr({ width: width, height: height });
     plotArea.attr('clip-path', 'url(#plotAreaClip)');
 
-    // Set scale domains
-    var maxDate = d3.max(data, function (d) {
-        return d.date;
-    });
+    function _updateScales(data) {
+        // Set scale domains
+        var maxDate = d3.max(data, function (d) {
+            return d.date;
+        });
 
-    xScale.domain([
-        new Date(maxDate.getTime() - (8.64e7 * 31.5)),
-        new Date(maxDate.getTime() + 8.64e7)
-    ]);
+        var minute = 6e4; // in milliseconds
+        xScale.domain([
+            new Date(maxDate.getTime() - minute * 15),
+            new Date(maxDate.getTime() + minute / 2)
+        ]);
 
-    var xDomain = xScale.domain();
-    var range = moment().range(xDomain[0], xDomain[1]);
-    var displayedData = [];
-    for (var i = 0; i < data.length; i += 1) {
-        if (range.contains(data[i].date)) {
-            displayedData.push(data[i]);
+        var xDomain = xScale.domain();
+        var range = moment().range(xDomain[0], xDomain[1]);
+        var displayedData = [];
+        for (var i = 0; i < data.length; i += 1) {
+            if (range.contains(data[i].date)) {
+                displayedData.push(data[i]);
+            }
         }
+
+        yScale.domain(
+            [
+                d3.min(displayedData, function (d) {
+                    return d.low - 0.2;
+                }),
+                d3.max(displayedData, function (d) {
+                    return d.high + 0.2;
+                })
+            ]
+        ).nice();
+
+        // Set scale ranges
+        xScale.range([0, width]);
+        yScale.range([height, 0]);
     }
 
-    yScale.domain(
-        [
-            d3.min(displayedData, function (d) {
-                return d.low;
-            }),
-            d3.max(displayedData, function (d) {
-                return d.high;
+    function drawAxesAndSeries(data) {
+        _updateScales(data);
+
+        // Draw axes
+        g.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis);
+
+        g.append('g')
+            .attr('class', 'y axis')
+            .call(yAxis);
+
+        // Draw series.
+        plotArea.append('g')
+            .attr('class', 'series')
+            .datum(data)
+            .call(series);
+    }
+
+    function updateAxesAndSeries(data) {
+        _updateScales(data);
+
+        // Update axes
+        g.select('.x.axis')
+            .call(xAxis);
+
+        g.select('.y.axis')
+            .call(yAxis);
+
+        // Update series
+        plotArea.select('.series')
+            .datum(data)
+            .call(series);
+    }
+
+    function processDatasetForChart() {
+        // If we've reached the length of the dataset, then reset the index
+        if (datasetIndex === dataset.length + 1) {
+            datasetIndex = 20;
+        }
+
+        // Get the first <datasetIndex> entries from the array
+        var data = [];
+        data = dataset.slice(0, datasetIndex += 1);
+
+        // Nest entries using their date as the key
+        data = d3.nest()
+            .key(function(d) {
+                return d.date;
             })
-        ]
-    ).nice();
+            .entries(data);
 
-    // Set scale ranges
-    xScale.range([0, width]);
-    yScale.range([height, 0]);
+        // If there are any duplicates, use the latest in the array
+        var dataWithEarliestDuplicatesRemoved = [];
+        data.forEach(function(entry) {
+            dataWithEarliestDuplicatesRemoved.push(entry.values[entry.values.length - 1]);        
+        });
 
-    // Draw axes
-    g.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis);
+        return dataWithEarliestDuplicatesRemoved;
+    }
 
-    g.append('g')
-        .attr('class', 'y axis')
-        .call(yAxis);
-
-    // Draw series.
-    plotArea.append('g')
-        .attr('class', 'series')
-        .datum(data)
-        .call(series);
+    // Update the data for the chart at a set time interval
+    setInterval(function() {
+        var data = processDatasetForChart();
+        updateAxesAndSeries(data);
+    }, 1000);
 });
