9c7ce66d39bc38a9ec30f340a588781fb8d37d32
James Leftley <jleftley@scottlogic.com>
2014-10-23 15:17:49 +0100

    Added Bootstrap, bootstrap-slider and dat.GUI

--- js/require.config.js
+++ js/require.config.js
@@ -1,17 +1,27 @@
 /* jshint unused:false */
 /* global require: true */
-
+'use strict';
 var require = {
     paths: {
         'd3': 'lib/d3',
         'jstat': 'lib/jstat',
         'moment': 'lib/moment',
         'moment-range': 'lib/moment-range',
-        'modernizr': 'lib/modernizr'
+        'modernizr': 'lib/modernizr',
+        'jquery': 'lib/jquery',
+        'jquery.bootstrap': 'lib/bootstrap',
+        'jquery.bootstrap-slider': 'lib/bootstrap-slider',
+        'GUI': 'lib/dat.gui'
     },
     shim: {
         'modernizr': {
             exports: 'Modernizr'
+        },
+        'jquery.bootstrap': {
+            deps:['jquery']
+        },
+        'jquery.bootstrap-slider': {
+            deps:['jquery', 'jquery.bootstrap']
         }
     },
     config: {
